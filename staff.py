0772dc6d9aff6bc256e10d4ceb79e7fb4ec07c9e
John Rinehart <johnrichardrinehart@gmail.com>
2018-09-25 12:23:54 -0700

    added ID counter and an abstract Staff class from which Nurse and CNA are derived

--- staff.py
+++ staff.py
@@ -1,47 +1,46 @@
-import sys
+import sys, abc
 
-class Nurse:
-    first = '';
-    last = '';
-    isCharge = 0;
-    seniority = -1;
-    daysRequested = [];
-    shiftsPerWeek = 3; # TODO: Change per nurse;
-    RO = [];
+class Staff(abc.ABC):
 
-    def __init__(self, fname, lname, isCharge, daysRequested, RO, seniority):
+    __id = 0;
+
+    @abc.abstractmethod
+    def __init__(self,fname, lname, daysRequested, daysVAC, daysRO, seniority=-1):
         self.first = fname;
         self.last = lname;
-        self.isCharge = 1 if isCharge.lower() == 'yes' else 0;
+
         self.daysRequested = list(map(lambda x: int(x), daysRequested.split(',')));
-        self.RO = list(map(lambda x: int(x), RO.split(','))) if RO != '' else []
+        self.daysVAC = list(map(lambda x: int(x), daysVAC.split(','))) if daysVAC != '' else [];
+        self.daysRO = list(map(lambda x: int(x), daysRO.split(','))) if daysRO != '' else []
+
         self.seniority = seniority;
 
-# From https://stackoverflow.com/questions/46851479/python-sort-list-with-two-arguments-in-compare-function
+        self.__id = self.incrementID(); # increments the counter for the class
+        self.daysScheduled = [];
+
+    @property
+    def id(self):
+        return self.__id
+
+
+    @classmethod
+    def incrementID(cls):
+        cls.__id += 1;
+        return cls.__id
+
+    # From https://stackoverflow.com/questions/46851479/python-sort-list-with-two-arguments-in-compare-function
     def __lt__(self, other):
         s = self.first.lower() + self.last.lower()
         o = other.first.lower() + other.last.lower()
         return s < o
 
-class CNA:
-    first = '';
-    last = '';
-    isCharge = 0;
-    seniority = -1;
-    daysRequested = [];
-    shiftsPerWeek = 3; # TODO: Change per nurse;
-    RO = [];
+# n.id is an incremented value for each Nurse object that is instantiated
+class Nurse(Staff):
+    def __init__(self, fname, lname, daysRequested, daysRO, daysVAC, seniority, isCharge, isVent):
+        Staff.__init__(self,fname, lname, daysRequested, daysVAC, daysRO, seniority)
+        self.isCharge = True if isCharge.lower() == 'yes' else False;
+        self.isVent = True if isCharge.lower() == 'yes' else False;
 
-    def __init__(self, fname, lname, isCharge, daysRequested, RO, seniority):
-        self.first = fname;
-        self.last = lname;
-        self.isCharge = 1 if isCharge.lower() == 'yes' else 0;
-        self.daysRequested = list(map(lambda x: int(x), daysRequested.split(',')));
-        self.RO = list(map(lambda x: int(x), RO.split(','))) if RO != '' else []
-        self.seniority = seniority;
-
-# From https://stackoverflow.com/questions/46851479/python-sort-list-with-two-arguments-in-compare-function
-    def __lt__(self, other):
-        s = self.first.lower() + self.last.lower()
-        o = other.first.lower() + other.last.lower()
-        return s < o
+class CNA(Staff):
+    def __init__(self, fname, lname, daysRequested, daysVAC, daysRO, seniority):
+        Staff.__init__(self,fname, lname, daysRequested, daysVAC, daysRO, seniority)
