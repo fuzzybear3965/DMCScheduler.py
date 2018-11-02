6908993b8a8bbd150056a1d5baeed399978c3140
John Rinehart <johnrichardrinehart@gmail.com>
2018-11-01 20:29:49 -0700

    restructured scheduleTable.js

--- front/js/scheduleTable.js
+++ front/js/scheduleTable.js
@@ -1,62 +1,39 @@
 var scheduleTable = new Tabulator('#schedule', {
     layout: 'fitColumns',
-   columns: (function() {
-            cols = [];
-            cols.push({
-                title: 'Name',
-                field: 'name',
-            })
-            for (i = 0; i < 28; i++) {
-                var day = '';
-                switch (i%7) {
-                    case 0:
-                        day = 'Su';
-                        break;
-                    case 1:
-                        day = 'M';
-                        break;
-                    case 2:
-                        day = 'T';
-                        break;
-                    case 3:
-                        day = 'W';
-                        break;
-                    case 4:
-                        day = 'R';
-                        break;
-                    case 5:
-                        day = 'F';
-                        break;
-                    case 6:
-                        day = 'Sa';
-                        break;
-                }
-                week = Math.floor(i/7);
+    columns: (function() {
+        cols = [];
+        cols.push({
+            title: 'Name',
+            field: 'name',
+        })
+        for (i = 0; i < 28; i++) {
+            var day = dayString(i);
+            week = Math.floor(i/7);
 
-                title_str = day + ' ' + week + ' (' + i + ')';
-                field_str = 'day' + i;
-                cols.push({
-                    title: title_str,
-                    field: field_str,
-                    align: 'center',
-                    bottomCalc: 'count',
-                    formatter: function(cell, params, onRendered) {
-                        pastel_red = '#FFC0CB';
-                        pastel_blue = '#AEC6CF';
-                        cell_value = cell.getValue();
-                        if (cell.getValue() === '7P(C)') {
-                            cell.getElement().style.background = pastel_red;
-                        } else if (cell.getValue() === '7P(V)') {
-                            cell.getElement().style.background = pastel_blue;
-                        } else if (cell.getValue() === '7P(C)(V)') {
-                            cell.getElement().style.background = `linear-gradient(to right, ${pastel_red} 50%, ${pastel_blue} 50%)`;
-                        }
-                        return cell.getValue();
-                    },
-                }); 
-            }
-            return cols
-        }()),
+            title_str = day + ' ' + week + ' (' + i + ')';
+            field_str = 'day' + i;
+            cols.push({
+                title: title_str,
+                field: field_str,
+                align: 'center',
+                bottomCalc: 'count',
+                formatter: function(cell, params, onRendered) {
+                    pastel_red = '#FFC0CB';
+                    pastel_blue = '#AEC6CF';
+                    cell_value = cell.getValue();
+                    if (cell.getValue() === '7P(C)') {
+                        cell.getElement().style.background = pastel_red;
+                    } else if (cell.getValue() === '7P(V)') {
+                        cell.getElement().style.background = pastel_blue;
+                    } else if (cell.getValue() === '7P(C)(V)') {
+                        cell.getElement().style.background = `linear-gradient(to right, ${pastel_red} 50%, ${pastel_blue} 50%)`;
+                    }
+                    return cell.getValue();
+                },
+            }); 
+        }
+        return cols
+    }()),
 })
 
 function insertSchedule(d) {
@@ -68,3 +45,22 @@ function downloadSchedule() {
 }
 downloadEl = document.getElementById('schedule-download');
 downloadEl.addEventListener('click', downloadSchedule);
+
+function dayString(i) {
+            switch (i%7) {
+                case 0:
+                    return 'Su';
+                case 1:
+                    return 'M';
+                case 2:
+                    return 'T';
+                case 3:
+                    return 'W';
+                case 4:
+                    return 'R';
+                case 5:
+                    return 'F';
+                case 6:
+                    return 'Sa';
+            }
+}
